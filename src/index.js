103735e7f48415be36f3a963eb4945972302c43b
Evan <schafer.evan.r@gmail.com>
2020-04-06 17:16:58 -0700

    Add functionality for video list and video player
    Video list now shows video thumbnails titles and descriptions, and
    the video player renders and plays a default video.
    
    Spec runner tests pass for videolist, videolistentry, videoplayer
    and App (minus rendering a new video when a video is clicked on)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // TODO: Render the `App` component to the DOM
+//debugger;
 import App from './components/App.js';
+ReactDOM.render(<App />, document.getElementById("app"));
 
+//ReactDOM.render(<App />, document.getElementById('app'));
